5846ea9258a49f23ff5729a18b458d48e7b43465
Emrie <cameronemrie1@gmail.com>
2024-09-24 00:37:45 -0500

    Polished movement, made general beat mechanic for 120bpm

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,25 +1,15 @@
 extends CharacterBody2D
 
+const SPEED = 500
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_d") - Input.get_action_strength("ui_a")
+	input_vector.y = Input.get_action_strength("ui_s") - Input.get_action_strength("ui_w")
+	input_vector = input_vector.normalized()
+	
+	if input_vector:
+		velocity = input_vector * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity = input_vector
 	move_and_slide()
