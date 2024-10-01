5451ea3c891940313a87283f324a528277e5751a
Aderohunmu02 <95999672+Aderohunmu02@users.noreply.github.com>
2024-10-01 14:07:05 -0500

    created the menu

---
+++ Scenes/CharacterScenes/menu.gd
@@ -0,0 +1,9 @@
+extends Control
+
+func _ready() -> void:
+	# This function is called when the node enters the scene tree
+	pass
+
+func _on_StartButton_pressed() -> void:
+	# Change the scene to the next one using the correct method for Godot 4.x
+	get_tree().change_scene_to_file("res://Scenes/FLOOR_1.tscn")
