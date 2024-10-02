38b74a291241c7b7a6c367c99edcfd97911b1c16
Emrie <cameronemrie1@gmail.com>
2024-10-02 12:16:40 -0500

    Revert "finished merge"
    This reverts commit 57beecd6c83f5ad13faaea40da0fd76e38d0c3c7.

--- Scenes/CharacterScenes/menu.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-func _ready() -> void:
-	# This function is called when the node enters the scene tree
-	pass
-
-func _on_StartButton_pressed() -> void:
-	# Change the scene to the next one using the correct method for Godot 4.x
-	get_tree().change_scene_to_file("res://Scenes/game.tscn")
